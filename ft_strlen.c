7eb454638dc211d912050bed4465f9fa701e529b
magnitopic <magnitopic@gmail.com>
2022-09-12 19:38:41 +0200

    strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,23 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alaparic <alaparic@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/12 16:32:59 by alaparic          #+#    #+#             */
-/*   Updated: 2022/09/12 16:33:01 by alaparic         ###   ########.fr       */
+/*   Created: 2022/09/12 18:59:30 by alaparic          #+#    #+#             */
+/*   Updated: 2022/09/12 18:59:31 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
+#include "header.h"
 
-int				ft_isalpha(int c);
-int				ft_isascii(int c);
-int				ft_isdigit(int c);
-int				ft_isdigit(int c);
-int				ft_isprint(int c);
-unsigned long	ft_strlen(char *s);
+unsigned long	ft_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/*
+int	main(void)
+{
+	char phrase[100]="Hello There";
+	printf("%lu\n", ft_strlen(phrase));
+	printf("%lu\n", strlen(phrase));
+	return (0);
+}
+*/
