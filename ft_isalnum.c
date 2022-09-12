c785d860970531f7780a199e95cde7e1855f4b23
magnitopic <magnitopic@gmail.com>
2022-09-12 19:42:28 +0200

    ft isalnum

---
+++ ft_isalnum.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaparic <alaparic@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/12 18:42:43 by alaparic          #+#    #+#             */
+/*   Updated: 2022/09/12 18:42:44 by alaparic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+int	ft_isalnum(int c)
+{
+	if (ft_isdigit(c) || ft_isalpha(c))
+		return (1);
+	return (0);
+}
+/*
+int	main(void)
+{
+	printf("%d\n", ft_isalnum('3'));
+	printf("%d\n", isalnum('3'));
+	return (0);
+}
+*/
