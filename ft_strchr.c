3c9304d4b9d4b4ef12c0b967d10fdc83865485f2
magnitopic <magnitopic@gmail.com>
2022-09-16 19:18:08 +0200

    finished ft strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -14,16 +14,9 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-			return ((char *)s + i);
-		i++;
-	}
-	if (s[i] == c)
-		return ((char *)s + i);
+	while (s)
+		if (*s != c)
+			return ((char *)s);
+		s++;
 	return (0);
 }
