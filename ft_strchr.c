738ceb2c7958292c4a27157976fec69bd0eeedc8
magnitopic <magnitopic@gmail.com>
2022-09-16 17:18:22 +0200

    ft strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,20 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/16 16:02:56 by alaparic          #+#    #+#             */
-/*   Updated: 2022/09/16 16:07:13 by alaparic         ###   ########.fr       */
+/*   Created: 2022/09/16 16:14:31 by alaparic          #+#    #+#             */
+/*   Updated: 2022/09/16 17:18:06 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_toupper(int c)
+char	*ft_strchr(const char *s, int c)
 {
-	if ((c >= 97 && c <= 122))
-		c -= 32;
-	return (c);
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return ((char *)s + i);
+		i++;
+	}
+	if (s[i] == c)
+		return ((char *)s + i);
+	return (0);
 }
--- ft_tolower.c
+++ ft_tolower.c
@@ -6,13 +6,13 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 16:03:41 by alaparic          #+#    #+#             */
-/*   Updated: 2022/09/16 16:04:21 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/09/16 16:12:33 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	ft_tolower(char c)
+int	ft_tolower(int c)
 {
 	if (c >= 65 && c <= 90)
 		c += 32;
