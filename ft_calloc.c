442cbc3e0682b386e2d5e3b05dcf079f044e308f
magnitopic <magnitopic@gmail.com>
2022-09-18 17:22:14 +0200

    ft calloc

---
+++ ft_calloc.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/18 16:48:21 by alaparic          #+#    #+#             */
+/*   Updated: 2022/09/18 17:21:17 by alaparic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
