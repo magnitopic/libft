a831749d58bd1a9ce03c0e7eef16d9572e73b1b2
magnitopic <magnitopic@gmail.com>
2022-09-18 18:21:22 +0200

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,19 +6,23 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 18:11:00 by alaparic          #+#    #+#             */
-/*   Updated: 2022/09/17 12:42:48 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/09/18 18:21:05 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    char *str;
+	char	*new_str;
+	size_t	i;
 
-    str= malloc(len);
-    while ()
-    {
-        
-    }
+	new_str = malloc(len * sizeof(char));
+	i = 0;
+	while (len > i && new_str != '\0')
+	{
+		new_str[i] = s[start];
+		i++;
+	}
+	return (new_str);
 }
