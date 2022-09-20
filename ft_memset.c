251079c9a113d0017f8ed48d853b048c3886e861
magnitopic <magnitopic@gmail.com>
2022-09-20 18:39:58 +0200

    fixed error on memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 19:44:50 by alaparic          #+#    #+#             */
-/*   Updated: 2022/09/17 12:42:27 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/09/20 18:39:44 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	*a;
+	unsigned char	c1;
 
 	a = b;
+	c1 = (unsigned char)c;
 	while (len > 0)
 	{
 		*a = c;
