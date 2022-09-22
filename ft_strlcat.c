a0f90a078e62d91abd68635f394f8a440f1ae54c
magnitopic <magnitopic@gmail.com>
2022-09-22 19:18:34 +0200

    fixed strlcat var name error

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,32 +6,32 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 18:42:32 by alaparic          #+#    #+#             */
-/*   Updated: 2022/09/18 19:57:44 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/09/22 19:06:43 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+int	ft_strlcat(char *dest, char *src, size_t size)
 {
 	size_t	dst_len;
 	size_t	src_len;
 	size_t	i;
 	size_t	j;
 
-	dst_len = ft_strlen(dst);
+	dst_len = ft_strlen(dest);
 	src_len = ft_strlen((char *)src);
 	i = 0;
 	j = dst_len;
-	if (dstsize == 0)
+	if (dst_len == 0)
 		return (src_len);
-	if (dstsize < dst_len)
-		return (src_len + dstsize);
-	while (src[j] && (dst_len + j) < dstsize)
-		dst[i++] = src[j++];
-	if ((dst_len + j) == dstsize && dst_len < dstsize)
-		dst[--i] = '\0';
+	if (dst_len < dst_len)
+		return (src_len + dst_len);
+	while (src[j] && (dst_len + j) < dst_len)
+		dest[i++] = src[j++];
+	if ((dst_len + j) == dst_len && dst_len < dst_len)
+		dest[--i] = '\0';
 	else
-		dst[i] = '\0';
+		dest[i] = '\0';
 	return (src_len + dst_len);
 }
