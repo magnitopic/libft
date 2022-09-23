94e6755248865a9695c213df9f5050022a590248
magnitopic <magnitopic@gmail.com>
2022-09-23 16:51:27 +0200

    fixed calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,17 +6,19 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 16:48:21 by alaparic          #+#    #+#             */
-/*   Updated: 2022/09/18 17:21:17 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/09/23 16:50:40 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
 	ptr = malloc(count * size);
-	ft_bzero(ptr, count * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, count);
 	return (ptr);
 }
