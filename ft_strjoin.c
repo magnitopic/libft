e6755fd89145408546701ce97f5ae684d322f132
magnitopic <magnitopic@gmail.com>
2022-09-27 19:43:19 +0200

    fixed strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 18:28:32 by alaparic          #+#    #+#             */
-/*   Updated: 2022/09/18 19:55:07 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/09/27 19:42:53 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,27 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int		len;
-	char	*concat;
+	char	*str;
+	int		a;
+	int		b;
+	int		i;
 
-	len = ft_strlen((char *)s1) + ft_strlen((char *)s2);
-	concat = malloc(len * sizeof(char));
-	concat = (char *)s1;
-	ft_strlcat(concat, s2, len);
-	return (concat);
+	if (!s1 || !s2)
+		return (NULL);
+	a = ft_strlen(s1);
+	b = ft_strlen(s2);
+	i = 0;
+	if (!(str = malloc(a + b + 1)) || !str)
+		return (NULL);
+	while (a--)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	a = i;
+	i = 0;
+	while (b--)
+		str[a++] = s2[i++];
+	str[a] = '\0';
+	return (str);
 }
