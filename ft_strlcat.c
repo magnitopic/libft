fad9c34fc4edd6228d06c38fa343c8c81524970a
magnitopic <magnitopic@gmail.com>
2022-09-27 17:19:12 +0200

    fixed strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 18:42:32 by alaparic          #+#    #+#             */
-/*   Updated: 2022/09/22 19:47:15 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/09/27 17:18:27 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,29 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	dst_len;
-	size_t	src_len;
-	size_t	i;
-	size_t	j;
+	char		*d;
+	const char	*s;
+	size_t		n;
+	size_t		dlen;
 
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen((char *)src);
-	i = 0;
-	j = dst_len;
-	if (dst_len == 0)
-		return (src_len);
-	if (dstsize < dst_len)
-		return (src_len + dstsize);
-	while (src[j] && (dst_len + j) < dstsize)
-		dst[i++] = src[j++];
-	if ((dst_len + j) == dstsize && dst_len < dstsize)
-		dst[--i] = '\0';
-	else
-		dst[i] = '\0';
-	return (src_len + dst_len);
+	d = dst;
+	s = src;
+	n = dstsize;
+	while (n-- != 0 && *d != '\0')
+		d++;
+	dlen = d - dst;
+	n = dstsize - dlen;
+	if (n == 0)
+		return (dlen + ft_strlen(s));
+	while (*s != '\0')
+	{
+		if (n != 1)
+		{
+			*d++ = *s;
+			n--;
+		}
+		s++;
+	}
+	*d = '\0';
+	return (dlen + (s - src));
 }
