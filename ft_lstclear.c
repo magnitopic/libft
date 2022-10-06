b1e36ef4d8c947a58b3d9c3fb12626f8e9ebbfcc
magnitopic <magnitopic@gmail.com>
2022-10-06 18:47:18 +0200

    fixed lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 09:33:06 by alaparic          #+#    #+#             */
-/*   Updated: 2022/10/05 09:41:23 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/10/06 18:42:44 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*lstcp;
 
-	while ((*lst)->next != 0)
+	if (lst && del)
 	{
-		lstcp = (*lst)->next;
-		ft_lstdelone((*lst), del);
-		*lst = lstcp;
+		while ((*lst) != 0)
+		{
+			lstcp = (*lst)->next;
+			ft_lstdelone((*lst), del);
+			*lst = lstcp;
+		}
 	}
-	ft_lstdelone((*lst), del);
-	*lst = NULL;
 }
