bbfe178ab2965d856c837109bb20cffb3d021dd8
magnitopic <magnitopic@gmail.com>
2023-03-04 16:43:06 +0100

    fixed error in comparing values

--- lists/ft_lstcompare.c
+++ lists/ft_lstcompare.c
@@ -6,7 +6,7 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 16:42:00 by alaparic          #+#    #+#             */
-/*   Updated: 2023/03/02 16:49:53 by alaparic         ###   ########.fr       */
+/*   Updated: 2023/03/04 16:42:15 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,9 @@ int	ft_lstcompare(t_list *lst1, t_list *lst2)
 	i = 0;
 	while (i < ft_lstsize(lst1))
 	{
-		if (ft_get(lst1, i)->content != ft_get(lst2, i)->content)
+		if (*(int *)ft_get(lst1, i)->content != *(int *)ft_get(lst2, i)->content){
 			return (0);
+		}
 		i++;
 	}
 	return (1);
